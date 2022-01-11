523842be4ea993c2c0f9a3b5477bcf7137586b8d
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-01-11 14:56:08 +0530

    Added The ProgramTest File

---
+++ ProgramTest/DataFormat.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramTest
+{
+    class DataFormat
+    {
+        public static void GetStudData(int i, double d, char c, bool b, string s, string l)
+        {
+            Console.WriteLine("------ Student Data ------");
+            Console.Write("Name : " + s +
+                          "\nRoll Number : " + i +
+                          "\nMy Marks : " + d +
+                          "\nMy location : " + l + "" +
+                          "\nMy Name Letter Start" + c + "" +
+                          "\nDid I Pass : " + b
+                          );
+            Console.WriteLine("\n");
+        }
+
+
+        public static void GetNumbersData(int i, long l, float f, double d, bool t, bool fa)
+        {
+            Console.WriteLine("------DataTypes ------");
+            Console.Write("Integer Value : " + i + "\nLong Value : " + l + "\nFloat Value : " + f + "\nDouble Value : " + d + "\nBoolean value : " + t + "\nBoolean Value : " + fa);
+            Console.WriteLine("\n");
+
+        }
+        public static void ConvertData(int i, double d, bool b, string s)
+        {
+            Console.WriteLine("------ Conversing Data ------");
+            Console.WriteLine("String Value :" + Convert.ToString(i));    // convert int to string
+            Console.WriteLine("Double Value :" + Convert.ToDouble(i));    // convert int to double
+            Console.WriteLine("Integer Value :" + Convert.ToInt32(d));  // convert double to int
+            Console.WriteLine("Long Value :" + Convert.ToInt64(d));  // convert double to long
+            Console.WriteLine("String Value :" + Convert.ToString(b));   // convert bool to string
+            Console.WriteLine("Boolean Value :" + Convert.ToBoolean(s));   // convert string to bool
+            Console.WriteLine("\n");
+
+        }
+
+        public static void GetFullName(string fName, string lName)
+        {
+            Console.WriteLine("------Full Name Using Concat and Interpolation ------");
+            //character or string to conacatinate a variable to another variable
+            string fullName = string.Concat(fName, lName);
+            Console.WriteLine("Full Name : " + fullName);
+
+            //String Interpolation
+            string name = $"My full name is: {fName} {lName}";
+            Console.WriteLine("Full Name : " + name);
+            Console.WriteLine("\n");
+        }
+
+        public static void GetFullName(string fName, int uAge)
+        {
+            Console.WriteLine("------ UserName With Age ------");
+            // Print the value of the variable which will display the input value
+            Console.WriteLine("Username is: " + fName + " \nAge is: " + uAge);
+            Console.WriteLine("\n");
+        }
+        public static int AddNumbers(int x, int y, int z)
+        {
+            return x + y + z;
+        }
+        public static int AddNumbers(int x, int y)
+        {
+            return x + y;
+        }
+        public static void MathOperator()
+        {
+            Console.WriteLine("------ Math Operators like min, max etc ------");
+            Console.Write("Maximum Value : " + Math.Max(5, 10));
+            Console.Write("\nMinimum Value : " + Math.Min(5, 10));
+            Console.Write("\nSquare Root Value : " + Math.Sqrt(64));
+            Console.Write("\nAbsolute Value : " + Math.Abs(-4.7));
+            Console.Write("\nRound Value : " + Math.Round(9.99));
+            Console.Write("\n");
+        }
+
+        public static void ArraySorting(string[] str, int[] arr)
+        {
+            Console.WriteLine("------ Sorting Data ------");
+            // Sort the arr from last to first.
+            // compare every element to each other
+            Array.Sort<string>(str, new Comparison<string>((i1, i2) => i2.CompareTo(i1)));
+
+            Console.WriteLine("Descending the string array : ");
+            // print all element of array
+            foreach (string value in str)
+            {
+                Console.Write(value + " ");
+            }
+            Console.WriteLine("\n");
+
+            // Sort array in ascending order.
+            Array.Sort(arr);
+
+            // print all element of array
+            Console.Write("Ascending Order : ");
+            foreach (int value in arr)
+            {
+                Console.Write(value + " ");
+            }
+            Console.WriteLine("\n");
+
+            // reverse array
+            Array.Reverse(arr);
+            Console.Write("Descending Order : ");
+            foreach (int value in arr)
+            {
+                Console.Write(value + " ");
+            }
+            Console.WriteLine("\n");
+
+
+            // compare every element to each other
+            Array.Sort<int>(arr, new Comparison<int>((i1, i2) => i2.CompareTo(i1)));
+
+            // print all element of array
+            Console.Write("descending order using comparision : ");
+            foreach (int value in arr)
+            {
+                Console.Write(value + " ");
+            }
+            Console.WriteLine("\n");
+        }
+
+        public static void GetArrayMath(int[] myNumbers)
+            {
+                Console.Write("Printing the array for min,max and sum : ");
+                //Finding the min max and sum
+                foreach (int value in myNumbers)
+                {
+                    Console.Write(value + " ");
+                }
+                Console.WriteLine("\n");
+                Console.WriteLine("Maximum Number in the array : " + myNumbers.Max());  // returns the largest value
+                Console.WriteLine("Minimum Number in the array : " + myNumbers.Min());  // returns the smallest value
+                Console.WriteLine("Sum Of Number in the array : " + myNumbers.Sum());  // returns the sum of elements
+                Console.ReadLine();
+            
+
+        }
+    }
+}
---
+++ ProgramTest/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramTest
+{
+
+    /// <summary>
+    /// Testing for Different DataTypes
+    ///   <br />
+    /// </summary>
+    public class Program
+    {
+      
+        //Main defines the entry point of the program
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter The Choices from below\n");
+                Console.Write("1: Printing Message \n2: Print Student Data \n3: Print Full Name and Age \n4: Number Format\n5: Data Conversion\n6: Math Operations \n7: Generating Random Number\n8: Array Sorting and Math Operations\n9: Addition of Number\n10: Exit Program\n");
+                int switchOptions = int.Parse(Console.ReadLine());
+                switch (switchOptions)
+                {
+                    case 1:
+                        //This message will displays in the console
+                        Console.WriteLine("Hello World!");  //This is a statement
+                        Console.WriteLine("I will print on a new line.");
+                        Console.Write("Hello World! ");
+                        Console.Write("I will print on the same line.");
+                        Console.Write("\n" + "\n");
+                        Console.Write("Enter The Message : ");
+                        string Message = Console.ReadLine();
+                        Console.Write("The Message is : " + Message);
+                        Console.Write("\n" + "\n");
+
+                        break;
+                    case 2:
+                        //User Data to store in method
+                        const string location = "Mumbai";
+                        DataFormat.GetStudData(44, 73.64D, 'R', true, "Raj Verma", location);
+                        break;
+                    case 3:
+                        //String method to store string and concat
+                        DataFormat.GetFullName("Raj", "Verma");
+                        //Taking input from the user
+                        Console.Write("Enter username : "); // Type your username and press enter
+                        string userName = Console.ReadLine();   // Create a  variable and get user input from the keyboard and store it in the variable
+                        Console.Write("Enter your age: ");
+                        int myAge = Convert.ToInt32(Console.ReadLine());  //console.readline returns a string
+                        DataFormat.GetFullName(userName, myAge);
+                        break;
+                    case 4:
+                        //The int long float bool and double data type
+                        DataFormat.GetNumbersData(100000, 1500000000L, 35e3F, 12E4D, true, false);
+                        Console.Write("\n");
+                        break;
+                    case 5:
+                        //Type Conversion Methods
+                        DataFormat.ConvertData(10, 5.25, true, "false");
+                        break;
+                    case 6:
+                        //Math Operator
+                        DataFormat.MathOperator();
+                        break;
+                    case 7:
+                        //Generating a random number
+                        Console.Write("Enter The Range In Which You Want Random Numbers : ");
+                        int ranNum = int.Parse(Console.ReadLine());
+                        Random random = new Random();
+                        Console.WriteLine(random.Next(ranNum + 1));
+                        break;
+                    case 8:
+                        // declaring and initializing the string array
+                        string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+                        int[] arr = new int[] { 1, 9, 6, 7, 5, 9 };
+                        int[] myNumbers = { 5, 1, 8, 9 };
+                        DataFormat.ArraySorting(cars, arr);
+                        DataFormat.GetArrayMath(myNumbers);
+                        break;
+                    case 9:
+                        //Adding numbers
+                        Console.Write("How many numbers you want to add : ");
+                        Console.WriteLine("1 : Three Numbers , 2 : Two Numbers");
+                        int numToAdd = int.Parse(Console.ReadLine());
+                        switch (numToAdd)
+                        {
+                            case 1:
+                                Console.Write("Enter the first number : ");
+                                int x = int.Parse(Console.ReadLine());
+                                Console.Write("Enter the second number : ");
+                                int y = int.Parse(Console.ReadLine());
+                                Console.Write("Enter the third number : ");
+                                int z = int.Parse(Console.ReadLine());
+                                Console.Write("Addition Of Three Numbers Is : " + DataFormat.AddNumbers(x, y, z));
+                                Console.ReadLine();
+                                break;
+                            case 2:
+                                Console.Write("Enter the first number : ");
+                                int x1 = int.Parse(Console.ReadLine());
+                                Console.Write("Enter the second number : ");
+                                int y2 = int.Parse(Console.ReadLine());
+                                Console.Write("Addition Of Two Numbers Is : " + DataFormat.AddNumbers(x1, y2));
+                                Console.ReadLine();
+                                break;
+                            default:
+                                Console.WriteLine("Enter The choice between 2 and 3");
+                                break;
+
+                        }
+                        break;
+                    case 10:
+                        Environment.Exit(0);
+                        return;
+
+
+                }
+            }
+            
+
+        }
+    }
+}
